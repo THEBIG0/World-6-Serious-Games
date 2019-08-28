cc5d02f9004b894b9a11d964bffa7c3617e7d067
THEBIG0 <owendcgunter@gmail.com>
2019-08-28 18:27:42 +0930

    PointAndClick and some art
    -Can click on object and currently prints out message (eventually want to use this to go to different rooms and to talk to people and interact with the game world)
    
    -Brought in some art for backgrounds and one character these are only placeholders and not final

---
+++ Assets/Scripts/PointAndClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("script pointAndClick running");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnClick();
+    }
+    
+    void OnClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("i clicked something");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit ))
+            {
+                if(hit.transform != null)
+                {
+                    Debug.Log(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+}
