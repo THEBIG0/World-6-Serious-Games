29c35d2117f1b575f1c16fe6cf9e9909da1e3680
THEBIG0 <owendcgunter@gmail.com>
2019-09-03 13:07:57 +0930

    Dialogue Changes when you reload scene
    Before when you went from kitchen to the minigame back to kitchen again the dialogue was still the same. Now with the DialogueSwitch script the dialogue now changes when you go back to the kitchen again (character says something new).

---
+++ Assets/Scripts/DialogueSwitch.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fungus;
+public class DialogueSwitch : MonoBehaviour
+{
+    public bool hasSaidBefore;
+    
+    public Flowchart flowchart;
+   string sceneName;
+   public View kitchenView;
+   public Camera mainCamera;
+   public GameObject characterModel;
+   public Character character;
+    private void Awake()
+    {
+     /*Scene currentScene = SceneManager.GetActiveScene(); 
+     sceneName = currentScene.name;
+     print(sceneName);
+     hasSaidBefore = flowchart.GetBooleanVariable("IfSaidBefore");
+     if (sceneName == "CookingGame")
+     {
+         hasSaidBefore = true;
+     }*/
+        
+        int numGameSessions = FindObjectsOfType<DialogueSwitch>().Length;
+
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+            Destroy(kitchenView);
+            Destroy(mainCamera);
+            Destroy(characterModel);
+            Destroy(character);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(kitchenView);
+            DontDestroyOnLoad(mainCamera);
+            DontDestroyOnLoad(characterModel);
+            DontDestroyOnLoad(character);
+        }
+        
+
+       
+       //print(hasSaidBefore);
+
+    }
+
+    void Update()
+    {
+        
+        Scene currentScene = SceneManager.GetActiveScene(); 
+        sceneName = currentScene.name;
+        hasSaidBefore = flowchart.GetBooleanVariable("IfSaidBefore");
+        print(hasSaidBefore);
+        if (sceneName == "CookingGame")
+        {
+            flowchart.SetBooleanVariable("IfSaidBefore", true);
+        }
+       
+    }
+}
