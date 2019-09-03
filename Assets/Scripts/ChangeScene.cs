708c9b844135fb49a33d976145b17e07f5091f01
THEBIG0 <owendcgunter@gmail.com>
2019-09-03 17:41:11 +0930

    Navigation System Implemented
    -Added Lounge Room (with new dialogue)
    -Added yellow arrows for each of the scenes include minigame
    -Bug: character model would appear in cooking game and i would also need to renable it afterwards (this has been resolved)

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
+    //public GameObject characterModel;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,12 +12,13 @@ public class ChangeScene : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         buttonPressed();
-    }
+      
+    }*/
     
-    void buttonPressed()
+    /*void buttonPressed()
     {
         if(Input.GetKeyDown(KeyCode.M))
         {
@@ -27,6 +29,28 @@ public class ChangeScene : MonoBehaviour
         {
             SceneManager.LoadScene(0);
         }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SceneManager.LoadScene(2);
+        }
         
+    }*/
+
+    public void LoadKitchen()
+    {
+        SceneManager.LoadScene(0);
+        //characterModel.SetActive(true);
+        //print(characterModel.activeSelf + " :isCharacterActive");
+    }
+
+   public void LoadLoungeRoom()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+   public void LoadCookingMiniGame()
+    {
+        SceneManager.LoadScene(1);
     }
 }
--- Assets/Scripts/DialogueSwitch.cs
+++ Assets/Scripts/DialogueSwitch.cs
@@ -6,13 +6,14 @@ using Fungus;
 public class DialogueSwitch : MonoBehaviour
 {
     public bool hasSaidBefore;
-    
+    //public bool isCharacterActive;
     public Flowchart flowchart;
    string sceneName;
    public View kitchenView;
    public Camera mainCamera;
    public GameObject characterModel;
    public Character character;
+   private int counter = 0;
     private void Awake()
     {
      /*Scene currentScene = SceneManager.GetActiveScene(); 
@@ -55,11 +56,38 @@ public class DialogueSwitch : MonoBehaviour
         Scene currentScene = SceneManager.GetActiveScene(); 
         sceneName = currentScene.name;
         hasSaidBefore = flowchart.GetBooleanVariable("IfSaidBefore");
+        //isCharacterActive = flowchart.GetBooleanVariable("IsCharacterActive");
         print(hasSaidBefore);
         if (sceneName == "CookingGame")
         {
+            counter = 0;
+            characterModel.SetActive(false);
+            print("character should be disabled");
+            counter = 1;
+        }
+        if (sceneName == "CookingGame" || sceneName == "Lounge-Room")
+        {
             flowchart.SetBooleanVariable("IfSaidBefore", true);
         }
+        else if (sceneName == "Kitchen")
+        {
+            flowchart.SetBooleanVariable("IfSaidBefore", false);
+            //flowchart.SetBooleanVariable("IsCharacterActive", true);
+            if (counter == 1)
+            {
+                //flowchart.SetBooleanVariable("IsCharacterActive", true);
+                characterModel.SetActive(true);
+                counter = 0;
+            }
+        }
+        
+        //bug: character model still appeared in the cooking game so i need to disable it then reenable it 
+        if (sceneName == "CookingGame")
+        {
+            characterModel.SetActive(false);
+            print("character should be disabled");
+        }
+
        
     }
 }
