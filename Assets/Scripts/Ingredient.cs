d267df1dfb7855f836ea93e378186c5da320c266
Unknown <sam32x@hotmail.com>
2019-09-04 23:59:49 +0930

    cooking minigame art + instructions

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour {
+    public Sprite[] sprites;
+    
+    void Start() {
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[(int)(Random.value * sprites.Length)];
+    }
+}
