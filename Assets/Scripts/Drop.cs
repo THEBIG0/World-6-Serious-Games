d47e3446d5a9ad69c5bb9f21cba6e61f8a1e2255
Unknown <sam32x@hotmail.com>
2019-09-12 07:53:04 +0930

    sandwich z order fixed, sandwich checker prototype started

--- Assets/Scripts/Drop.cs
+++ Assets/Scripts/Drop.cs
@@ -2,17 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drop : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class Drop : MonoBehaviour {
+    void Update() {
+        transform.position = new Vector3(transform.position.x, transform.position.y, -transform.position.y * 0.1f);     //z ordering
     }
 }
---
+++ Assets/Scripts/SandwichChecker.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandwichChecker : MonoBehaviour {
+    public GameObject text1;
+    void OnMouseDown() {
+        Object[] drops = GameObject.FindObjectsOfType(typeof(Drop));
+        if(drops.Length == 5) {
+            text1.GetComponent<UnityEngine.UI.Text>().text = "Well Done!";
+        } else {
+            text1.GetComponent<UnityEngine.UI.Text>().text = "Hmm...";
+        }
+    }
+}
