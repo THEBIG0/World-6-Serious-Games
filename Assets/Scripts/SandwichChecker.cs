c12ed71cc0dd1294b7575436eb47748ccac163bb
Unknown <sam32x@hotmail.com>
2019-09-20 09:27:43 +0930

    sandwich checker more advanced

--- Assets/Scripts/SandwichChecker.cs
+++ Assets/Scripts/SandwichChecker.cs
@@ -5,11 +5,44 @@ using UnityEngine;
 public class SandwichChecker : MonoBehaviour {
     public GameObject text1;
     void OnMouseDown() {
-        Object[] drops = GameObject.FindObjectsOfType(typeof(Drop));
-        if(drops.Length == 5) {
-            text1.GetComponent<UnityEngine.UI.Text>().text = "Well Done!";
-        } else {
-            text1.GetComponent<UnityEngine.UI.Text>().text = "Hmm...";
+        int score = 0;
+        MonoBehaviour[] drops = (MonoBehaviour[])GameObject.FindObjectsOfType(typeof(Drop));
+        //order drops by y coord, lowest first
+        for (int p = 0; p < drops.Length; p++) {
+            for (int i = 0; i < drops.Length - 1; i++) {
+                if (drops[i].transform.position.y > drops[i + 1].transform.position.y) {    //bubble? sort
+                    MonoBehaviour t = drops[i];
+                    drops[i] = drops[i + 1];
+                    drops[i + 1] = t;
+                }
+            }
         }
+        if (drops.Length == 5) {        //if correct number of ingredients placed
+            int breads = 0;
+            for(int i = 0; i < drops.Length; i++) { //count all the bread in the scene
+                if(drops[i].name.Substring(0, 5) == "bread") {
+                    breads++;
+                }
+            }
+            if(breads == 2) {           //if 2 slices of bread and 5 ingredients total, add one point
+                score++;
+                Debug.Log("bread is there");
+            }
+        }
+        int misaligned = 0;
+        for(int i = 1; i < drops.Length; i++) {     //check how many ingredients are misaligned with the first one placed
+            float tolerance = 1.2f;
+            if(drops[i].transform.position.x > drops[0].transform.position.x + tolerance || drops[i].transform.position.x < drops[0].transform.position.x - tolerance) {
+                misaligned++;
+            }
+        }
+        if(misaligned == 0 && drops.Length > 2) {       //give a point if all ingredients are lined up and there's at least 3 of them
+            score++;
+            Debug.Log("ingredients lined up");
+        }
+        score++;    //todo: check that the ingredients match the ones on the side, just giving a point for now
+        string[] texts = { "Absolutely not...", "Hmm...", "Not quite...", "Well Done!" };
+        text1.GetComponent<UnityEngine.UI.Text>().text = texts[score];
+        Debug.Log(score);
     }
 }
