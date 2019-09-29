f5cf1e79e227d2b29522cf66b4ae2a8cdc847e53
Unknown <sam32x@hotmail.com>
2019-09-29 19:51:15 +0930

    started the shopping minigame

---
+++ Assets/Scripts/ShoppingScroll.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingScroll : MonoBehaviour {
+
+    void Update() {
+        transform.Translate(Vector2.left * Time.deltaTime * 6f);
+        if(transform.position.x < -25) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ShoppingSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShoppingSpawner : MonoBehaviour {
+    public GameObject spawner;
+
+
+    private void Start() {
+        InvokeRepeating("spawn", 0f, 3.5f);
+        InvokeRepeating("spawn", 1.5f, 5.5f);      //some sort of random-ish seeming spawn timers
+    }
+
+    void spawn() {
+        int r = (int)Random.Range(0f, 2.99f);
+        GameObject s = Instantiate(spawner, transform.position, transform.rotation);
+        s.transform.position = new Vector3(12, 3 + (r * 1.5f), 0);
+    }
+}
