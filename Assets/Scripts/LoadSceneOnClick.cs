561734b0d97e29d5a8f7ef6d6dcb4087224b8da6
Unknown <sam32x@hotmail.com>
2019-11-05 13:59:26 +1030

    control buttons for minigames

---
+++ Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class LoadSceneOnClick : MonoBehaviour {
+    public int scene;
+    void OnMouseDown() {
+        SceneManager.LoadScene(scene);
+    }
+}
